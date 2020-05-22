1b799bf6ba6950171dcb477eb690e7650deb67f6
VKarpick <vincentkarpick@gmail.com>
2020-05-22 00:42:57 -0700

    Implement remove_child in Node class

--- checkers/src/main.cpp
+++ checkers/src/main.cpp
@@ -3,9 +3,22 @@
 #include "ai/train_checkers.h"
 
 
-//TODO does policy need environment if td learning has it?
 int main()
 {
-    TrainCheckers training(6, 1, true, "weights.dat", "weights.dat");
-    training.train();
+    //TrainCheckers training(6, 1, true, "weights.dat", "weights.dat");
+    /*TrainCheckers training(1, 2, true, "", "");
+    training.train();*/
+
+    /*Node<int> node(1);
+    Node<int> node2(2);
+
+    node.add_child2(node2);
+    std::cout << node.children_nodes().size() << std::endl;
+
+    node.remove_child(node2);
+    std::cout << node.children_nodes().size() << std::endl;
+    std::cout << (node2.parent() == nullptr) << std::endl;*/
+
+
+    //TODO replace pointers with references
 }
--- checkers/src/tree/node.h
+++ checkers/src/tree/node.h
@@ -6,10 +6,7 @@
 template <typename T>    // allow for data of any type to be provided to a node
 class Node {
 public:
-	Node(const T data) {
-		data_ = data;
-		parent_ = nullptr;
-	}
+	Node(const T data) : data_(data) {}
 
 	// getters
 	T data() { return data_; }
@@ -18,14 +15,26 @@ public:
 
 	// setter
 	void parent(Node<T>* new_parent) {
-		//TODO allow parent to change
-		if (parent_ == nullptr) parent_ = new_parent;
+		// have to remove this node as member of previous parent's children
+		if (parent_ != nullptr) parent_->remove_child(this);
+		parent_ = new_parent;
 	}
 
 	void add_child(Node<T>* child) {
 		if (child->parent() == nullptr) {
 			child->parent(this);
 			children_.push_back(child);
+		}
+	}
+
+	// removes first instance of child from the parent's children vector
+	void remove_child(Node<T>* node) {
+		for (auto child = children_.begin(); child != children_.end(); ++child) {
+			if (*child == node) {
+				children_.erase(child);
+				node->parent(nullptr);    // node should no longer have this as parent
+				break;
+			}
 		}
 	}
 
