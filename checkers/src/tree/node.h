81a80883f7186203e3a063eaa0aa92328748f8c4
VKarpick <vincentkarpick@gmail.com>
2020-05-23 16:35:04 -0700

    Update parameter names and add_child in node

--- checkers/src/tree/node.h
+++ checkers/src/tree/node.h
@@ -6,6 +6,7 @@
 template <typename T>    // allow for data of any type to be provided to a node
 class Node {
 public:
+	// constructors
 	Node(const T data) : data_(data) {}
 
 	// getters
@@ -13,18 +14,15 @@ public:
 	std::vector<Node<T>*> children() { return children_; }
 	Node<T>* parent() { return parent_; }
 
-	// setter
+	// setters
 	void parent(Node<T>* new_parent) {
-		// have to remove this node as member of previous parent's children
-		if (parent_ != nullptr) parent_->remove_child(this);
+		if (parent_ != nullptr) parent_->remove_child(this);    // have to remove this node as member of previous parent's children
 		parent_ = new_parent;
 	}
 
 	void add_child(Node<T>* child) {
-		if (child->parent() == nullptr) {
-			child->parent(this);
-			children_.push_back(child);
-		}
+		child->parent(this);
+		children_.push_back(child);
 	}
 
 	// removes first instance of child from the parent's children vector
