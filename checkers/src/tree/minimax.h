62d24ff2d4c5deb4a708c82b9d30093c915f247a
VKarpick <vincentkarpick@gmail.com>
2020-05-23 20:31:20 -0700

    Remove unneccesary pointers from minimax

--- checkers/src/tree/minimax.h
+++ checkers/src/tree/minimax.h
@@ -19,42 +19,47 @@ public:
 	Minimax(std::function<double(Node<T>*)> computeNodeValue, int maxDepth = 0, 
 		const std::function<void(Node<T>*)> extendTree = [](Node<T>*) {}) {
 
-		// function to calculate the value of a given node
 		computeNodeValue_ = computeNodeValue;
-		
 		maxDepth_ = maxDepth;
-
-		// function to find children of a node when passing incomplete tree
-		// if complete tree is provided, can be ignored
 		extendTree_ = extendTree;
 	}
 
 
-	MinimaxPair<T>* minimax(Node<T>* node, int currentDepth = 0, bool isMaxPlayer = true, 
+	MinimaxPair<T> minimax(Node<T>* node, int currentDepth = 0, bool isMaxPlayer = true, 
 		double alpha = -DBL_MAX, double beta = DBL_MAX) {
 		
-		if (currentDepth == maxDepth_) return new MinimaxPair<T>{ computeNodeValue_(node), node };
+		if (currentDepth == maxDepth_) return MinimaxPair<T>{ computeNodeValue_(node), node };
 		if (node->getChildren().empty()) extendTree_(node);
 
 		if (isMaxPlayer) {
-			MinimaxPair<T>* bestPair = new MinimaxPair<T>{ -DBL_MAX, nullptr };
-			int i = 0;
+			MinimaxPair<T> bestPair{ -DBL_MAX, nullptr };
+			int i{ 0 };
 			for (auto child : node->getChildren()) {
-				MinimaxPair<T>* childPair = minimax(child, currentDepth + 1, false, alpha, beta);
+				MinimaxPair<T> childPair{ minimax(child, currentDepth + 1, false, alpha, beta) };
+
 				// use >= to avoid returning nullptr when children exist
-				if (childPair->value >= bestPair->value) bestPair = new MinimaxPair<T>{ childPair->value, child };
-				alpha = fmax(alpha, bestPair->value);
+				if (childPair.value >= bestPair.value) {
+					bestPair.value = childPair.value;
+					bestPair.node = child;
+				}
+
+				alpha = fmax(alpha, bestPair.value);
 				if (beta <= alpha) break;
 			}
 			return bestPair;
 		}
 		else {
-			MinimaxPair<T>* bestPair = new MinimaxPair<T>{ DBL_MAX, nullptr };
+			MinimaxPair<T> bestPair{ DBL_MAX, nullptr };
 			for (auto child : node->getChildren()) {
-				MinimaxPair<T>* childPair = minimax(child, currentDepth + 1, true, alpha, beta);
+				MinimaxPair<T> childPair{ minimax(child, currentDepth + 1, true, alpha, beta) };
+				
 				// use <= to avoid returning nullptr when children exist
-				if (childPair->value <= bestPair->value) bestPair = new MinimaxPair<T>{ childPair->value, child };
-				beta = fmin(beta, bestPair->value);
+				if (childPair.value <= bestPair.value) {
+					bestPair.value = childPair.value;
+					bestPair.node = child;
+				}
+				
+				beta = fmin(beta, bestPair.value);
 				if (beta <= alpha) break;
 			}
 			return bestPair;
@@ -65,19 +70,24 @@ public:
 	double minimaxValue(Node<T>* node, int currentDepth = 0, bool isMaxPlayer = true, 
 		double alpha = -DBL_MAX, double beta = DBL_MAX) {
 		
-		return minimax(node, currentDepth, isMaxPlayer, alpha, beta)->value;
+		return minimax(node, currentDepth, isMaxPlayer, alpha, beta).value;
 	}
 
 
 	Node<T>* minimaxNode(Node<T>* node, int currentDepth = 0, bool isMaxPlayer = true, 
 		double alpha = -DBL_MAX, double beta = DBL_MAX) {
 		
-		return minimax(node, currentDepth, isMaxPlayer, alpha, beta)->node;
+		return minimax(node, currentDepth, isMaxPlayer, alpha, beta).node;
 	}
 
 
 private:
 	int maxDepth_;
-	std::function<void(Node<T>*)> extendTree_;
+
+	// function to calculate the value of a given node
 	std::function<double(Node<T>*)> computeNodeValue_;
+
+	// function to find children of a node when passing incomplete tree
+	// if complete tree is provided, can be ignored
+	std::function<void(Node<T>*)> extendTree_;
 };
