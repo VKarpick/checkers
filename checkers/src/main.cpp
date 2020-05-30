198c58088edcaf64a83559771b8ce1a1bf527cb3
VKarpick <vincentkarpick@gmail.com>
2020-05-30 16:25:32 -0700

    Trained additional 30 episodes with max depth 6 and step size 0.1

--- checkers/src/main.cpp
+++ checkers/src/main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    CheckersTrainer checkers_trainer(20, 6, 0.1);
+    CheckersTrainer checkers_trainer(30, 6, 0.1);
     //CheckersTrainer checkers_trainer(std::vector<double>(128, 2.0), 100, 4);
     //CheckersTrainer checkers_trainer(1, 1);
     checkers_trainer.train();
